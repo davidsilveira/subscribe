880c962ff4a941d0b284d3f7255e17f33e7dc95b
David Silveira <david@dsilveira.eti.br>
2026-09-24 15:49:09 -0300

    Adjust taxes calculation in receipt

--- lib/sales_taxes/receipt.rb
+++ lib/sales_taxes/receipt.rb
@@ -17,22 +17,33 @@ module SalesTaxes
 
     def to_s
       lines = items.map { |item| item_line(item) }
-      lines << format("Sales Taxes: %.2f", taxes_cents / 100.0)
+      lines << format("Sales Taxes: %.2f", total_taxes_cents / 100.0)
       lines << format("Total: %.2f", total_cents / 100.0)
       "#{lines.join("\n")}\n"
     end
 
     def item_line(item)
-      total = item.quantity * item.price
+      total = item.quantity * item.price + taxes_cents(item)
       format("%d %s: %.2f", item.quantity, item.name, total / 100.0)
     end
 
     def total_cents
-      items.sum { |item| item.quantity * item.price } + taxes_cents
+      items.sum { |item| item.quantity * item.price } + total_taxes_cents
     end
 
-    def taxes_cents
-      790
+    def total_taxes_cents
+      items.sum do |item|
+        taxes_cents(item)
+      end
+    end
+
+    def taxes_cents(item)
+      SalesTaxes::TaxCalculator.new(
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+        imported: item.imported?
+      ).tax_cents
     end
   end
 end
--- spec/sales_taxes/receipt_spec.rb
+++ spec/sales_taxes/receipt_spec.rb
@@ -3,23 +3,39 @@ require_relative '../../lib/sales_taxes/receipt'
 
 RSpec.describe SalesTaxes::Receipt do
   describe '#call' do
-    context 'when the input is valid' do
+    context 'when the input 1 is valid' do
       let(:expected_output) do
         <<~OUTPUT
-          1 imported bottle of perfume: 27.99
-          1 bottle of perfume: 18.99
+          2 book: 24.98
+          1 music CD: 16.49
+          1 chocolate bar: 0.85
+          Sales Taxes: 1.50
+          Total: 42.32
+        OUTPUT
+      end
+
+      it 'returns the correct output' do
+        input = SalesTaxes::InputParser.parse File.open("spec/fixtures/input1.txt").read
+
+        expect(described_class.call(input)).to eq(expected_output)
+      end
+    end
+
+    context 'when the input 3 is valid' do
+      let(:expected_output) do
+        <<~OUTPUT
+          1 imported bottle of perfume: 32.19
+          1 bottle of perfume: 20.89
           1 packet of headache pills: 9.75
-          3 imported boxes of chocolates: 33.75
+          3 imported boxes of chocolates: 35.55
           Sales Taxes: 7.90
           Total: 98.38
         OUTPUT
       end
 
-      let(:input) do
-       SalesTaxes::InputParser.parse File.open("spec/fixtures/input3.txt").read
-      end
-
       it 'returns the correct output' do
+        input = SalesTaxes::InputParser.parse File.open("spec/fixtures/input3.txt").read
+
         expect(described_class.call(input)).to eq(expected_output)
       end
     end
